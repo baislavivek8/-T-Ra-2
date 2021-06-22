04e8441695330365688d46fb164ff8641e90d6e2
Harshit Wadhawan <harshit.wadhawan@skilrock.com>
2021-06-23 02:02:45 +0530

    prod alb rule updated

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,21 @@
 BOLD='\033[1m'
 CYAN='\033[1;36m'
 
-echo -e "${CYAN}${BOLD}Please select the environment you want to deploy"
+echo -e "${CYAN}${BOLD}Please select the environment you want to deploy !\n\n1. UAT \n2. PROD"
 read -p "Environment :" env
+echo -e "${CYAN}${BOLD}Please provide the absolute path of the Terraform Variables file"
+read -p "Terraform Variables File Path :" env-path
 
-        if [ $env=='uat' ]
+        if [[ $env == "UAT" ]]
         then
-                cd /tmp ; git clone - b uat https://gitlab.com/harshit.wadhawan/                                                                                                             terraform.git ;
-                #terraform apply --var-file=uat-vars.tfvars
+                cd /tmp ; git clone -b uat git@gitlab.com:harshit.wadhawan/terraform.git
+                terraform plan --var-file=$env-path
                 exit 0
 
-        elif [ $env=='prod' ]
+        elif [[ $env == "PROD" ]]
         then
-                cd /tmp ; git clone - b prod https://gitlab.com/harshit.wadhawan/                                                                                                             /terraform.git ;
-                #terraform apply --var-file=prod-vars.tfvars
+                cd /tmp ; git clone -b prod git@gitlab.com:harshit.wadhawan/terraform.git
+                terraform plan --var-file=$env-path
                 exit 0
         else
                         exit 1
